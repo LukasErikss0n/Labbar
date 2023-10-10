ce7a4c37b578fbf9269eb7cdc0f214dccaac84dd
LukasErikss0n <Lukas.emil.eriksson@outlook.com>
2023-10-10 10:35:10 +0200

    alias funkar som den ska

--- labb04/Phonebook.py
+++ labb04/Phonebook.py
@@ -10,7 +10,7 @@ class telefonbok:
                             "save" : self.save, 
                             "load" : self.load, 
                             "show" : self.show,
-                            "delet" : self.delet
+                            "delete" : self.delete
                             }
             
             while True:
@@ -25,17 +25,18 @@ class telefonbok:
                     break
                 
         def checkIfExists(self, inputName, inputNumber):
-            for name, number in self.listDic.items(): #skapar name och number för varje element i dictonarin
-                if inputName == name: #tittar om inputname matchar name från listan
-                    return True, number, 1
-                elif inputNumber == number: #tittar om inputnumber matchar number från listan
-                    return True, 2
+            for number, names in self.listDic.items(): #skapar name och number för varje element i dictonarin
+                for name in names : 
+                    if inputName == name: #tittar om inputname matchar name från listan
+                        return True, number, 1
+                    elif inputNumber == number: #tittar om inputnumber matchar number från listan
+                        return True, 2
                 
         def add(self, inputName, inputNumber):
-            existOrNot = self.checkIfExists( inputName, inputNumber) #får värdet true med 1 om namnet finns, får värder 2 och true om number redan finns
+            existOrNot = self.checkIfExists(inputName, inputNumber) #får värdet true med 1 om namnet finns, får värder 2 och true om number redan finns
 
             if not existOrNot: #tittar om inget finns
-                self.listDic[inputName] = inputNumber
+                self.listDic[inputNumber] = [inputName]
             elif existOrNot[1] == 2: #om number finns, för specifika felmedelande
                 print("number alredy exists")
             elif existOrNot[2] == 1: #om namn finns, för specifika felmedelande
@@ -45,22 +46,24 @@ class telefonbok:
         def lookup(self, inputName):
             getNumber = self.checkIfExists( inputName, 0) #får ut numret om namnet finns
             if  getNumber:
-                print("Phone number for " + inputName + ": " + getNumber[1]) 
+                print("Phone number for " + inputName + ": " + getNumber[1])
             else:
                 print("The name" + inputName + " does not exist")
 
         def alias(self, inputName, inputAlias):
             getNumber = self.checkIfExists(inputName, 0)
             if getNumber:
-                self.listDic[inputAlias] = getNumber[1] #skapar ett alias för inputname 
+                self.listDic[getNumber[1]] += [inputAlias]  #skapar ett alias för inputname 
             else:
-                print("The name " + inputName + "does not exist")
-
+                print("The name " + inputName + " does not exist")
+    
         def change(self, inputName, inputNewNumber):
             existsOrNot = self.checkIfExists(inputName, inputNewNumber)
             if existsOrNot: #tittar om namnet finns
                 if existsOrNot[1] != 2:
-                    self.listDic.update({inputName: inputNewNumber}) #bytter numret för inputname
+                    values = self.listDic[existsOrNot[1]] #sparar infon i dictonaryn för nyckeln inputname
+                    del self.listDic[existsOrNot[1]] #tar bort gamla nyckel paret från dictionary
+                    self.listDic[inputNewNumber] = values
                 else:
                     print("Number already exists")
                 
@@ -70,17 +73,20 @@ class telefonbok:
         def quit(self):
             raise SystemExit #stänger av programmet 
         
-        def show(self):
-            print("Phone list:")
-            for name, number in self.listDic.items(): #visar hela telefonboken
-                print(name + ": " + number)
+        def show(self, promts):
+            if promts == "list":
+                print("Phone list:")
+                for number, names in self.listDic.items(): #visar hela telefonboken
+                    for name in names:
+                        print(name + ": " + number)
         
         def save(self, saveAs):
-            for name, number in self.listDic.items(): #för varje namn pch number i dictonaryn ska det spara det till filen
-                txtFile = saveAs + ".txt"
-                f = open(txtFile, "a")
-                f.write(number + ";" + name + ";\n")
-                f.close
+            for number, names in self.listDic.items(): #för varje namn pch number i dictonaryn ska det spara det till filen
+                for name in names:
+                    txtFile = saveAs + ".txt"
+                    f = open(txtFile, "a")
+                    f.write(number + ";" + name + ";\n")
+                    f.close
         
         def load(self, loadFile):
             txtFile = loadFile + ".txt"
@@ -96,20 +102,29 @@ class telefonbok:
             while i < len(saved): #loopar igenom saved
                 nameAndNumber = saved[i]
                 seperate = nameAndNumber.split(";", 2) #separar name and number
-                self.listDic[seperate[1]] = seperate[0]
+                self.listDic[seperate[0]] = [seperate[1]]
                 print(seperate[1] + ": " + seperate[0])
                 i += 1
+            f.close
                 
-        def delet(self, typeOfDelet, deletFile ):
-            if typeOfDelet == "phonebok":
-                txtFile = deletFile + ".txt"
+        def delete(self, typeOfDelete, deleteFileOrName ):
+            if typeOfDelete == "phonebok":
+                txtFile = deleteFileOrName + ".txt"
                 import os
                 if os.path.exists(txtFile):
-                    os.remove(txtFile)
+                    os.remove(txtFile) #raderar filen med namnet txtFile
                 else:
                     print("The file does not exist")
+            elif typeOfDelete == "name": 
+                existsOrNot = self.checkIfExists(deleteFileOrName, 0)
+                if existsOrNot:
+                    del self.listDic[existsOrNot[1]] #raderar namnet och numret från dictonaryn
+                else:
+                    print("The " + deleteFileOrName + " do not exist")
             else:
-                print("this type of " + typeOfDelet + " delet does not exist")
-                        
+                print("this type of " + typeOfDelete + " delete does not exist")
+                
+        
+            
 telefonbok()
         
